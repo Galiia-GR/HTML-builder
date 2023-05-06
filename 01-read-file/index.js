46ed1a5b3ce20cb525139cbf1c96d4d0051734fd
Galiia-GR <hope05590@gmail.com>
2023-05-06 22:28:18 +0600

    feat: resolve task number 1

---
+++ 01-read-file/index.js
@@ -0,0 +1,15 @@
+const fs =require('fs');
+const path = require('path');
+const {stdout} = process;
+
+const readingFile = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+readingFile.on('err', (err) => {
+  console.log ('opps, something went wrong', err);
+}
+);
+
+readingFile.on('data', data => {
+  stdout.write(data);
+}
+);
