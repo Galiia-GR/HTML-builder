87cd74ac0310608cbce9aec20bc263a254e51899
Galiia-GR <hope05590@gmail.com>
2023-05-08 01:30:40 +0600

    feat:some functions in task3

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,20 +1,41 @@
 const fs = require('fs');
 const path = require('path');
+
 const {stdout,exit} = process;
 
 const direct = path.join(__dirname, 'files');
 const newDirect=path.join(__dirname, 'files-copy');
 
-function copy() {
-  if (newDirect) {
-    stdout.write(`такая папка уже создана ${newDirect}`);
-  }
-  fs.mkdir(path.join(newDirect), (err)=> {
+
+function makeFolder() {
+  fs.mkdir((newDirect),{ recursive: false }, (err) => {
     if (err) {
       throw err;
+
     }
   });
- exit();
 }
 
-copy();
+fs.access(newDirect, (err) => {
+  if (err) {
+    stdout.write(`\n такая папка уже создана ${newDirect}`);
+    throw err;
+  }
+  else {
+    makeFolder();
+  }
+});
+
+
+fs.readdir(direct, (err, list)=>{
+  if (err) {
+    throw err;
+  }
+  list.forEach((el) => {
+    fs.copyFile((direct, el), (newDirect, el)), (err) => {
+      if (err) {
+        throw err;
+      }
+    };
+  });
+});
