861dac90bf7b3fcde90ad2392a230fa89dc960ae
Galiia-GR <hope05590@gmail.com>
2023-05-07 22:38:12 +0600

    feat: solution task3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,20 +1,29 @@
 const fs = require('fs');
 const path = require('path');
-const folder= 'secret-folder';
+const {stdout,exit} = process;
+const folderS = 'secret-folder';
 
-fs.readdir(path.join(__dirname, folder), (err, files) => {
+fs.readdir(path.join(__dirname, folderS), (err, files) => {
   if (err) {
     throw err;
   }
 
-  files.forEach((el) => {
-    fs.stat(path.join(__dirname, folder, el), (err, el)=>{
+  files.forEach((file) => {
+    fs.stat(path.join(__dirname, folderS, file), (err, el)=>{
       if (err){
         throw err;
       }
       if(el.isFile()) {
+        const name = path.basename(file, '').split('.')[0];
+        const extend = path.basename(file, '').split('.')[1];
+        const size = el.size/1024;
+        stdout.write(`${name} - ${extend} - ${size}kb`);
       }
     });
   });
 }
 );
+
+process.on('SIGINT', () => {
+  exit();
+});
