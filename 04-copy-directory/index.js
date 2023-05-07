4190f19815578992d927292e17364eac7727ef06
Galiia-GR <hope05590@gmail.com>
2023-05-08 00:35:42 +0600

    feat: start task4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const {stdout,exit} = process;
 
+const direct = path.join(__dirname, 'files');
+const newDirect=path.join(__dirname, 'files-copy');
+
+function copy() {
+  if (newDirect) {
+    stdout.write(`такая папка уже создана ${newDirect}`);
+  }
+  fs.mkdir(path.join(newDirect), (err)=> {
+    if (err) {
+      throw err;
+    }
+  });
+ exit();
+}
+
+copy();
