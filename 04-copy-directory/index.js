05bf4aff00c60f97310e77a54f7c28129da6e704
Galiia-GR <hope05590@gmail.com>
2023-05-08 16:15:01 +0600

    fix: overwriting folder task4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,36 +3,39 @@ const path = require('path');
 const fsPromises = fs.promises;
 
 
-const {stdout,exit} = process;
+const {stdout} = process;
 
 const direct = path.join(__dirname, 'files');
 const newDirect=path.join(__dirname, 'files-copy');
 
 
 fsPromises.access(newDirect). then(function() {
-  stdout.write(`\n такая папка уже создана ${newDirect}`);
-  exit();
+  stdout.write(`Данная папка уже создана ${newDirect} \n`);
+  stdout.write('Перезаписываю файлы...\n');
+  copyFile();
 }).catch(function() {
   stdout.write('Создаю папку...\n');
+  copyFile();
 });
 
+function copyFile() {
+  fsPromises.mkdir(newDirect, {recursive:true}). then(function() {
 
-fsPromises.mkdir(newDirect, {recursive:true}). then(function() {
+    fs.readdir(direct, (err, files)=> {
+      files.forEach((el) => {
+        if (err) throw err;
 
-  fs.readdir(direct, (err, files)=> {
-    files.forEach((el) => {
-      if (err) throw err;
-
-      fs.copyFile(
-        path.join(direct, el),
-        path.join(newDirect,el),
-        (err) => {
-          if (err) throw err;
-          console.log('Файл скопирован');
-        }
-      );
+        fs.copyFile(
+          path.join(direct, el),
+          path.join(newDirect,el),
+          (err) => {
+            if (err) throw err;
+            console.log('Файл скопирован');
+          }
+        );
+      });
     });
+  }).catch(function() {
+    console.log('Не удалось скопирвать файлы');
   });
-}).catch(function() {
-  console.log('Не удалось скопирвать файлы');
-});
+}
