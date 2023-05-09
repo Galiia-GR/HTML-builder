7822208448d0ef3747c01ac12046cc07d42d720a
Galiia-GR <hope05590@gmail.com>
2023-05-09 22:25:16 +0600

    refactor:html is work task6

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,3 +1,4 @@
+
 const fs = require('fs');
 const path = require('path');
 const fsPromises = fs.promises;
@@ -6,52 +7,13 @@ const {stdout} = process;
 
 const newDirect=path.join(__dirname, 'project-dist');
 
-fsPromises.access(newDirect). then(function() {
-  stdout.write(`Данная папка уже создана ${newDirect} \n`);
-  stdout.write('Перезаписываю файлы...\n');
-  buildFile();
-}).catch(function() {
-  stdout.write('Создаю папку...\n');
-  buildFile();
-});
 
-
-////import css
-const dirCssBundle = path.join(__dirname, 'project-dist', 'bundle.css');
-const dirStyle = path.join(__dirname, 'styles');
-
-function buildFile() {
-  fsPromises.mkdir(newDirect, {recursive:true}, (err) => {
-    if (err) throw err;
-  });
-
-  fs.readdir(dirStyle, (err, files) => {
-    if (err) throw err;
-
-    files.forEach((file) => {
-      fs.stat(path.join(__dirname, 'styles', file), (err, el) => {
-        if (err) throw err;
-
-        const extend = path.basename(file, '').split('.')[1];
-
-        if(el.isFile() && extend === 'css') {
-          const fileIn = fs.createReadStream(path.join(__dirname, 'styles', file), 'utf-8');
-          fileIn.on('data', data => {
-            fs.appendFile(dirCssBundle, data, (err) => {
-              if (err) throw err;
-            });
-          });
-        }
-      });
-    });
-  });
-}
+stdout.write('Перезаписываю файлы...\n');
 
 
 //////copyAssets
 const dirBundle = path.join(__dirname, 'project-dist', 'assets');
 const dirAssets = path.join(__dirname, 'assets');
-
 
 const copyDir = (dirAssets, dirBundle) => {
   fs.mkdir(dirBundle, { recursive: true }, (err) => {
@@ -81,9 +43,36 @@ const copyDir = (dirAssets, dirBundle) => {
   });
 };
 
-copyDir(dirAssets, dirBundle);
 
 
+////import css
+const dirCssBundle = path.join(__dirname, 'project-dist', 'style.css');
+const dirStyle = path.join(__dirname, 'styles');
+
+fsPromises.mkdir(newDirect, {recursive:true}, (err) => {
+  if (err) throw err;
+});
+
+fs.readdir(dirStyle, (err, files) => {
+  if (err) throw err;
+
+  files.forEach((file) => {
+    fs.stat(path.join(__dirname, 'styles', file), (err, el) => {
+      if (err) throw err;
+
+      const extend = path.basename(file, '').split('.')[1];
+
+      if(el.isFile() && extend === 'css') {
+        const fileIn = fs.createReadStream(path.join(__dirname, 'styles', file), 'utf-8');
+        fileIn.on('data', data => {
+          fs.appendFile(dirCssBundle, data, (err) => {
+            if (err) throw err;
+          });
+        });
+      }
+    });
+  });
+});
 
 
 ////work with html
@@ -128,3 +117,4 @@ fs.readdir(compDirFile, (err, files) => {
   });
 });
 
+copyDir(dirAssets, dirBundle);
